b73fc21f18a7f9be3f1e4805be0bf324013be66e
SusimaraRoberti <susimararoberti@outlook.com>
2022-11-18 14:31:19 -0300

    feat: inserido a animação no background da home

--- teste-tecnico-frontend/src/pages/home/styles.js
+++ teste-tecnico-frontend/src/pages/home/styles.js
@@ -1,4 +1,22 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const colorsAnimation = keyframes`
+  0% {
+    background-position: 0% 25%;
+  }
+  25% {
+    background-position: 50% 25%;
+  }
+  50%{
+    background-position: 75% 50%;
+  }
+  75% {
+    background-position: 100% 75%;
+  }
+  100%{
+    background-position: 0% 25%;
+  }
+`;
 
 export const StyledHome = styled.main`
   display: flex;
@@ -8,5 +26,9 @@ export const StyledHome = styled.main`
   width: 100%;
   min-height: 100vh;
   background: linear-gradient(45deg, #faf7f0, #cdfcf6, #bccef8, #98a8f8);
-  background-size: 150% 150%;
+  background-size: 200% 200%;
+  animation-name: ${colorsAnimation};
+  animation-duration: 15s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease;
 `;
